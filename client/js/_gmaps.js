928107f13db7891234867c0c70b9fec1168bcb44
redcap3000 <redcap3000@gmail.com>
2013-10-05 15:46:12 -0700

    buncha changes adding gmaps libraries and helpers

---
+++ client/js/_gmaps.js
@@ -0,0 +1,67 @@
+/*
+ * instageo / geoserve - Ronaldo Barbachano 2013
+ * http://redcapmedia.com
+ 
+ BEGIN GMAPS
+ *
+ *
+ */
+
+
+
+Meteor.startup(function(){
+
+gmapsMarkers = [],infoWindows = [],locationsMarkers = [],
+closeInfoWindows = function(){if(infoWindows.length > 0)
+            return infoWindows.filter(function(arr){
+               arr.close();
+               return false;
+            });},
+createMap = function(latLng) {
+    var mapOptions = {
+        disableDoubleClick: true,
+        streetViewControl: false,
+        scrollwheel: false,
+        zoom: 15,
+        center: latLng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+},
+placeNavMarker = function(latLng,title) {
+    if(typeof title == 'undefined'){
+        title = 'untitled';
+    }
+
+    var new_marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        'title': title
+        });
+    
+    /*var infoWindow = new google.maps.InfoWindow({
+        content: '<div class="infoWindow"><p>'+title+'</p></div>'
+    });
+    
+    infoWindows.push(infoWindow);
+    
+    google.maps.event.addListener(new_marker, 'click', function() {
+        closeInfoWindows();
+        // how to issue template event?
+    };
+    */
+    gmapsMarkers.push(new_marker);
+},
+// takes either array with two integers (x,y) or a google maps LatLng object.
+setMapCenter = function(q){
+    if(typeof map == 'undefined' && Session.get('access_token')){
+        createMap();
+    }
+    map.setCenter((typeof q == 'object' && q.length == 2? new google.maps.LatLng(q[0],q[1]) : (typeof q == 'object' ? q: new google.maps.LatLng(0,0))));
+}
+ });
+/*
+ *
+ * END GMAPS
+ *
+ */
--- client/js/main.js
+++ client/js/main.js
@@ -1,19 +1,6 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to stayfit.";
-  };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+Template.map.rendered = function(){
+        if(typeof map == 'undefined'){
+            console.log('creating map')
+            createMap();
+        }
 }
